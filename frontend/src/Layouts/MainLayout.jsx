8a4676aa88857483b0dfb001ef45fc7e08c5b021
Barış Muharrem Ateş <barisates_2002@hotmail.com>
2024-08-19 09:31:48 +0300

    Search added

--- frontend/src/Layouts/MainLayout.jsx
+++ frontend/src/Layouts/MainLayout.jsx
@@ -1,17 +1,21 @@
-﻿import React from "react";
-import Footer from "../Components/Layout/Footer/Footer";
+﻿import Footer from "../Components/Layout/Footer/Footer";
 import Header from "../Components/Layout/Header/Header";
 import Policy from "../Components/Layout/Policy/Policy";
 import Proptypes from "prop-types";
+import Search from "../Components/Modals/Search/Search";
+import { useState } from "react";
 
 const MainLayout = ({ children }) => {
+  const [isSearch, setIsSearch] = useState(false);
+
   return (
-    <React.Fragment>
-      <Header />
+    <div className="main-layout">
+      <Search isSearch={isSearch} setIsSearch={setIsSearch} />
+      <Header setIsSearch={setIsSearch} />
       {children}
       <Policy />
       <Footer />
-    </React.Fragment>
+    </div>
   );
 };
 
